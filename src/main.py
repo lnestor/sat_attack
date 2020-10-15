29cf98396c83a87aa14bf18fa4ec1c8ff02aabe1
Lucas Nestor <lucas15nestor@gmail.com>
2020-10-15 15:26:21 -0400

    Remove old main

--- src/main.py
+++ src/main.py
@@ -1,32 +1,10 @@
-from z3 import *
-
-from tokenizer import Tokenizer
-from parser import Parser
-from circuit_builder import CircuitBuilder
+from circuit import Circuit
 from circuit_solver import solve_ckt
-
-def extract_values_from_model(model, key_input_names, p_input_names):
-    """
-    Extracts the boolean values for each input from a given model from the z3 solver.
-    The objects returned from solver.solve() are a special z3 format, so this function
-    converts them into a more useable form.
-
-    model: the model returned from a z3 solver
-    key_input_names: the names of key inputs
-    p_input_names: the names of primary inputs
-
-    returns: values of key inputs, values of primary inputs
-    """
-
-    key_input_vals = []
-    p_input_vals = []
-    for i in model:
-        if str(i) in key_input_names:
-            key_input_vals.append(model[i])
-        else:
-            p_input_vals.append(model[i])
-
-    return key_input_vals, p_input_vals
+from dip_finder import DipFinder
+from key_space import KeySpace
+from tokenizer import Tokenizer
+from oracle_runner import OracleRunner
+from parser import Parser
 
 def read_benchmark(filename):
     """
@@ -34,46 +12,48 @@ def read_benchmark(filename):
 
     filename: the name of the benchmark file
 
-    returns: nodes of circuit, name of output nodes
+    returns: object representation of the circuit
     """
     with open(filename) as f:
         t = Tokenizer(f)
         p = Parser()
         nodes, output_names = p.parse(t)
-        return nodes, output_names
+        return Circuit(nodes, output_names)
 
-def run_oracle(filename, input_names, input_values):
-    # TODO: this doesn't really need to be it's own function I think
-    nodes, output_names = read_benchmark(filename)
-    builder = CircuitBuilder()
-    oracle_ckt, input_names, _ = builder.build(nodes, output_names)
-    return solve_ckt(oracle_ckt, input_names, input_values)
+def find_incorrect_keys(locked_ckt, key_space, oracle_output, dip):
+    incorrect_keys = []
 
-def prune_keys(nodes, output_names, oracle_outputs, p_input_names, p_input_vals):
-    locked_ckt, p_input_names, key_input_names = builder.build(nodes, output_names)
+    for key_vector in key_space:
+        locked_output = solve_ckt(locked_ckt, {**dip, **key_vector})
 
-    for i in range(2**len(key_input_names)):
-        key_input_values = [int(i / (2**name_index) % 2) == 0 for name_index, _ in enumerate(key_input_names)]
-        locked_outputs = solve_ckt(locked_ckt, key_input_names + p_input_names, key_input_values + p_input_vals)
+        if locked_output != oracle_output:
+            incorrect_keys.append(key_vector)
 
-        if locked_outputs != oracle_outputs:
-            print(locked_outputs)
-            # These keys are incorrect, prune them
-
+    return incorrect_keys
 
 if __name__ == "__main__":
-    nodes, output_names = read_benchmark("benchmarks/sample_multiple_outputs.v")
+    locked_ckt = read_benchmark("benchmarks/sample_multiple_outputs.v")
+    key_space = KeySpace(locked_ckt)
+    finder = DipFinder(locked_ckt)
 
-    builder = CircuitBuilder()
-    miter_ckt, p_input_names, key_input_names = builder.build_miter(nodes, output_names)
+    oracle_ckt = read_benchmark("benchmarks/sample_multiple_outputs_oracle.v")
+    runner = OracleRunner(oracle_ckt)
 
-    s = Solver()
-    s.add(miter_ckt == True)
-    s.check() # TODO: handle when this doesn't work
+    while len(key_space) > 1:
+        print("=== Iteration ===")
+        dip = finder.find_dip()
+        print("DIP: " + str(dip))
 
-    key_input_vals, p_input_vals = extract_values_from_model(s.model(), key_input_names, p_input_names)
+        oracle_output = runner.run(dip)
+        print("Oracle: " + str(oracle_output))
 
-    oracle_outputs = run_oracle("benchmarks/sample_multiple_outputs_oracle.v", p_input_names, p_input_vals)
-    print(oracle_outputs)
+        incorrect_keys = find_incorrect_keys(locked_ckt, key_space, oracle_output, dip)
+        print("Incorrect keys: " + str(incorrect_keys))
 
-    prune_keys(nodes, output_names, oracle_outputs, p_input_names, p_input_vals)
+        key_space.remove(incorrect_keys)
+        finder.mark_incorrect_keys(incorrect_keys)
+
+        print("")
+
+    for i in key_space:
+        print(i)
